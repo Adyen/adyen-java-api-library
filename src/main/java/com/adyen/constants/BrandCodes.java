5f11fe3c50f6b59a276877c262ce649afbeb2340
Giorgos Adam <georgios.adam@adyen.com>
2017-08-03 11:14:14 +0200

    Add brand codes as constants

---
+++ src/main/java/com/adyen/constants/BrandCodes.java
@@ -0,0 +1,55 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Java API Library
+ *
+ * Copyright (c) 2017 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+package com.adyen.constants;
+
+public interface BrandCodes {
+    String PAYPAL = "paypal";
+    String PAYPAL_ECS = "paypal_ecs";
+    String KLARNA = "klarna";
+    String AFTERPAY = "afterpay_default";
+    String SEPA_DIRECT_DEBIT = "sepadirectdebit";
+    String IDEAL = "ideal";
+    String CHINA_UNION_PAY = "cup";
+    String CARTEBANCAIRE = "cartebancaire";
+    String VISA = "visa";
+    String MASTERCARD = "mc";
+    String UATP = "uatp";
+    String AMEX = "amex";
+    String MAESTRO = "maestro";
+    String MAESTRO_UK = "maestrouk";
+    String DINERS = "diners";
+    String DISCOVER = "discover";
+    String VISA_DANKORT = "visadankort";
+    String JCB = "jcb";
+    String LASER = "laser";
+    String VIAS = "vias";
+    String SOLO = "solo";
+    String BCMC = "bcmc";
+    String BIJCARD = "bijcard";
+    String DANKORT = "dankort";
+    String HIPERCARD = "hipercard";
+    String ELO = "elo";
+    String VISA_ALPHABANK_BONUS = "visaalphabankbonus";
+    String MC_ALPHABANK_BONUS = "mcalphabankbonus";
+    String KARENMILLEN = "karenmillen";
+    String OASIS = "oasis";
+    String WAREHOUSE = "warehouse";
+}
