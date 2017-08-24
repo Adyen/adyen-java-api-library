80bc0cc892b600ff19be5900576736393b22e5d3
Giorgos Adam <georgios.adam@adyen.com>
2017-08-24 15:18:57 +0200

    Fixes #49, Use correct datetime format for ISO-8601

--- src/main/java/com/adyen/serializer/DateSerializer.java
+++ src/main/java/com/adyen/serializer/DateSerializer.java
@@ -23,17 +23,19 @@ package com.adyen.serializer;
 import java.lang.reflect.Type;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
 public class DateSerializer implements JsonSerializer<Date> {
-
     public static final String DATE_FORMAT = "yyyy-MM-dd";
 
+    @Override
     public JsonElement serialize(Date date, Type typeOfSrc, JsonSerializationContext context) {
         SimpleDateFormat formatter = new SimpleDateFormat(DATE_FORMAT);
+        formatter.setTimeZone(TimeZone.getTimeZone("GMT"));
         return new JsonPrimitive(formatter.format(date));
     }
 }
