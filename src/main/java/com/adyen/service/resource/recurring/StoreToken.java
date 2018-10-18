b9bf01e28339fa7d25d8ed47eab3edfc66e42b20
Ashwin Gururaja <agururaj@adobe.com>
2018-10-18 16:01:48 -0700

    Add StoreToken API implementation

---
+++ src/main/java/com/adyen/service/resource/recurring/StoreToken.java
@@ -0,0 +1,37 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Java API Library
+ *
+ * Copyright (c) 2017 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+
+package com.adyen.service.resource.recurring;
+
+import java.util.Arrays;
+
+import com.adyen.Client;
+import com.adyen.Service;
+import com.adyen.service.Resource;
+
+public class StoreToken extends Resource {
+
+    public StoreToken(Service service) {
+        super(service, service.getClient().getConfig().getEndpoint() + "/pal/servlet/Recurring/" + Client.RECURRING_API_VERSION + "/storeToken",
+                Arrays.asList("merchantAccount", "recurring.contract", "shopperReference"));
+    }
+
+}
