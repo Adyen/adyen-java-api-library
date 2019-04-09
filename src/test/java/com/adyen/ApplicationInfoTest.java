d0c38f3b322841a6f1dad6653c921ef76cad4f4c
renatoma <renato.martins@adyen.com>
2019-04-09 11:42:59 +0200

    [PW-828] Add application info to adyen-java-api-library - Add default library name + version / private set

--- src/test/java/com/adyen/ApplicationInfoTest.java
+++ src/test/java/com/adyen/ApplicationInfoTest.java
@@ -1,27 +1,23 @@
 package com.adyen;
 
-import org.junit.Test;
 import com.adyen.model.applicationinfo.ApplicationInfo;
-import com.adyen.model.applicationinfo.CommonField;
+import org.junit.Test;
+
 import static com.adyen.Client.LIB_NAME;
 import static com.adyen.Client.LIB_VERSION;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class ApplicationInfoTest {
 
     @Test
     public void TestApplicationInfoEquals() {
-
-        CommonField adyenLibrary = new CommonField();
-        adyenLibrary.setName(LIB_NAME);
-        adyenLibrary.setVersion(LIB_VERSION);
-
         ApplicationInfo applicationInfo = new ApplicationInfo();
-        applicationInfo.setAdyenLibrary(adyenLibrary);
-
         ApplicationInfo applicationInfo2 = new ApplicationInfo();
-        applicationInfo2.setAdyenLibrary(adyenLibrary);
 
-        assertTrue(applicationInfo.equals(applicationInfo2));
+        assertEquals(applicationInfo, applicationInfo2);
+        assertNotNull(applicationInfo.getAdyenLibrary());
+        assertEquals(LIB_NAME, applicationInfo.getAdyenLibrary().getName());
+        assertEquals(LIB_VERSION, applicationInfo.getAdyenLibrary().getVersion());
     }
 }
