5a4b83a145d7b17ae6e040780c108879c1c9381c
Renato Martins <renato.martins@adyen.com>
2019-08-28 11:41:13 +0200

    [PW-1625] Add connectedTerminals API endpoint (#238)
    * Add connectedTerminals API endpoint
    
    * connectedTerminals tests

---
+++ src/main/java/com/adyen/service/PosPayment.java
@@ -0,0 +1,58 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Java API Library
+ *
+ * Copyright (c) 2019 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+package com.adyen.service;
+
+import com.adyen.ApiKeyAuthenticatedService;
+import com.adyen.Client;
+import com.adyen.model.terminal.ConnectedTerminalsRequest;
+import com.adyen.model.terminal.ConnectedTerminalsResponse;
+import com.adyen.service.exception.ApiException;
+import com.adyen.service.resource.terminal.cloud.ConnectedTerminals;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+
+public class PosPayment extends ApiKeyAuthenticatedService {
+
+    private ConnectedTerminals connectedTerminals;
+
+    public PosPayment(Client client) {
+        super(client);
+        connectedTerminals = new ConnectedTerminals(this);
+    }
+
+    /**
+     * POST /connectedTerminals API call
+     *
+     * @param connectedTerminalsRequest ConnectedTerminalsRequest
+     * @return ConnectedTerminalsResponse
+     * @throws IOException  IOException
+     * @throws ApiException ApiException
+     */
+    public ConnectedTerminalsResponse connectedTerminals(ConnectedTerminalsRequest connectedTerminalsRequest) throws IOException, ApiException {
+        String jsonRequest = GSON.toJson(connectedTerminalsRequest);
+        String jsonResult = connectedTerminals.request(jsonRequest);
+        ConnectedTerminalsResponse connectedTerminalsResponse = GSON.fromJson(jsonResult, new TypeToken<ConnectedTerminalsResponse>() {
+        }.getType());
+
+        return connectedTerminalsResponse;
+    }
+}
---
+++ src/main/java/com/adyen/service/resource/terminal/cloud/ConnectedTerminals.java
@@ -0,0 +1,34 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Java API Library
+ *
+ * Copyright (c) 2019 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+package com.adyen.service.resource.terminal.cloud;
+
+import com.adyen.Service;
+import com.adyen.service.Resource;
+
+import java.util.Collections;
+
+public class ConnectedTerminals extends Resource {
+    public ConnectedTerminals(Service service) {
+        super(service,
+              service.getClient().getConfig().getTerminalApiCloudEndpoint() + "/connectedTerminals",
+                Collections.singletonList("merchantAccount"));
+    }
+}
