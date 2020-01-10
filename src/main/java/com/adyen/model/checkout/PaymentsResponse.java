7e34085073954f51c88ea4ec60089ef49adac218
rkewlani <kewlani.rajni@gmail.com>
2020-01-10 09:50:15 +0100

    [PW-1792]:removed unused import

--- src/main/java/com/adyen/model/checkout/PaymentsResponse.java
+++ src/main/java/com/adyen/model/checkout/PaymentsResponse.java
@@ -30,7 +30,6 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
