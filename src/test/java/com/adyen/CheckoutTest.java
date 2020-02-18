423f1e67ceca62475dec2d04bea56f849741bb33
renatoma <renato.martins@adyen.com>
2020-02-18 10:59:58 +0100

    Added test

--- src/test/java/com/adyen/CheckoutTest.java
+++ src/test/java/com/adyen/CheckoutTest.java
@@ -349,6 +349,36 @@ public class CheckoutTest extends BaseTest {
     }
 
     @Test
+    public void TestRecurringPaymentMethodDetails() {
+        DefaultPaymentMethodDetails defaultPaymentMethodDetails = new DefaultPaymentMethodDetails();
+        defaultPaymentMethodDetails.setStoredPaymentMethodId("testStoredPaymentMethodId");
+
+        PaymentsRequest paymentsRequest = createPaymentsCheckoutRequest();
+        paymentsRequest.setPaymentMethod(defaultPaymentMethodDetails);
+
+        String jsonRequest = PRETTY_PRINT_GSON.toJson(paymentsRequest);
+
+        assertEquals("{\n"
+                + "  \"amount\": {\n"
+                + "    \"value\": 1000,\n"
+                + "    \"currency\": \"USD\"\n"
+                + "  },\n"
+                + "  \"merchantAccount\": \"MagentoMerchantTest\",\n"
+                + "  \"paymentMethod\": {\n"
+                + "    \"storedPaymentMethodId\": \"testStoredPaymentMethodId\"\n"
+                + "  },\n"
+                + "  \"reference\": \"Your order number\",\n"
+                + "  \"returnUrl\": \"https://your-company.com/...\",\n"
+                + "  \"applicationInfo\": {\n"
+                + "    \"adyenLibrary\": {\n"
+                + "      \"name\": \"" + LIB_NAME + "\",\n"
+                + "      \"version\": \"" + LIB_VERSION + "\"\n"
+                + "    }\n"
+                + "  }\n"
+                + "}",jsonRequest );
+    }
+
+    @Test
     public void TestDateSerializers() throws ParseException {
         PaymentsRequest paymentsRequest = new PaymentsRequest();
 
