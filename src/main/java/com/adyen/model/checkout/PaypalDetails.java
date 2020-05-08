4cee298155892e37ffebe0f63a3e699e4947ecc5
alessio <alessio.zampatti@adyen.com>
2020-05-08 11:24:16 +0200

    PW-2289: added PaypalDetails

---
+++ src/main/java/com/adyen/model/checkout/PaypalDetails.java
@@ -0,0 +1,162 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Java API Library
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+
+package com.adyen.model.checkout;
+
+import java.util.Objects;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+
+import java.io.IOException;
+
+/**
+ * PaypalDetails
+ */
+
+public class PaypalDetails implements OneOfPaymentRequestPaymentMethod {
+
+    @SerializedName("type")
+    private String type = "paypal";
+
+    @JsonAdapter(SubtypeEnum.Adapter.class)
+    public enum SubtypeEnum {
+        SDK("sdk"),
+        REDIRECT("redirect");
+
+        private String value;
+
+        SubtypeEnum(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return String.valueOf(value);
+        }
+
+        public static SubtypeEnum fromValue(String text) {
+            for (SubtypeEnum b : SubtypeEnum.values()) {
+                if (String.valueOf(b.value).equals(text)) {
+                    return b;
+                }
+            }
+            return null;
+        }
+
+        public static class Adapter extends TypeAdapter<SubtypeEnum> {
+            @Override
+            public void write(final JsonWriter jsonWriter, final SubtypeEnum enumeration) throws IOException {
+                jsonWriter.value(enumeration.getValue());
+            }
+
+            @Override
+            public SubtypeEnum read(final JsonReader jsonReader) throws IOException {
+                String value = jsonReader.nextString();
+                return SubtypeEnum.fromValue(String.valueOf(value));
+            }
+        }
+    }
+
+    @SerializedName("subtype")
+    private SubtypeEnum subtype = null;
+
+    public PaypalDetails type(String type) {
+        this.type = type;
+        return this;
+    }
+
+    /**
+     * **paypal**
+     *
+     * @return type
+     **/
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public PaypalDetails subtype(SubtypeEnum subtype) {
+        this.subtype = subtype;
+        return this;
+    }
+
+    public SubtypeEnum getSubtype() {
+        return subtype;
+    }
+
+    public void setSubtype(SubtypeEnum subtype) {
+        this.subtype = subtype;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PaypalDetails paypalDetails = (PaypalDetails) o;
+        return Objects.equals(this.subtype, paypalDetails.subtype) &&
+                Objects.equals(this.type, paypalDetails.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subtype, type);
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class PaypalDetails {\n");
+
+        sb.append("    subtype: ").append(toIndentedString(subtype)).append("\n");
+        sb.append("    type: ").append(toIndentedString(type)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+
+}
