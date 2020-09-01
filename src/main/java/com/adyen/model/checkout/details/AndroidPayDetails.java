994846dac7da24b8be30fa54f3057bf4f509ccd5
saquib <saquib.sayyad@adyen.com>
2020-09-01 17:39:50 +0200

    PW-2807 Update to checkout version 64 and add missing classes and fields

---
+++ src/main/java/com/adyen/model/checkout/details/AndroidPayDetails.java
@@ -0,0 +1,116 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Java API Library
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+
+package com.adyen.model.checkout.details;
+
+import com.adyen.model.checkout.PaymentMethodDetails;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Objects;
+
+/**
+ * AndroidPayDetails
+ */
+
+public class AndroidPayDetails implements PaymentMethodDetails {
+  @SerializedName("androidPayToken")
+  private String androidPayToken = null;
+
+  @SerializedName("type")
+  private String type = null;
+
+  public AndroidPayDetails androidPayToken(String androidPayToken) {
+    this.androidPayToken = androidPayToken;
+    return this;
+  }
+
+   /**
+   * Get androidPayToken
+   * @return androidPayToken
+  **/
+  public String getAndroidPayToken() {
+    return androidPayToken;
+  }
+
+  public void setAndroidPayToken(String androidPayToken) {
+    this.androidPayToken = androidPayToken;
+  }
+
+  public AndroidPayDetails type(String type) {
+    this.type = type;
+    return this;
+  }
+
+   /**
+   * The payment method type.
+   * @return type
+  **/
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    AndroidPayDetails androidPayDetails = (AndroidPayDetails) o;
+    return Objects.equals(this.androidPayToken, androidPayDetails.androidPayToken) &&
+        Objects.equals(this.type, androidPayDetails.type);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(androidPayToken, type);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class AndroidPayDetails {\n");
+
+    sb.append("    androidPayToken: ").append(toIndentedString(androidPayToken)).append("\n");
+    sb.append("    type: ").append(toIndentedString(type)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+
+}
---
+++ src/main/java/com/adyen/model/checkout/details/MobilePayDetails.java
@@ -0,0 +1,93 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Java API Library
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+
+package com.adyen.model.checkout.details;
+
+import com.adyen.model.checkout.PaymentMethodDetails;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Objects;
+/**
+ * MobilePayDetails
+ */
+
+public class MobilePayDetails implements PaymentMethodDetails {
+  @SerializedName("type")
+  private String type = "mobilepay";
+
+  public MobilePayDetails type(String type) {
+    this.type = type;
+    return this;
+  }
+
+   /**
+   * **mobilepay**
+   * @return type
+  **/
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    MobilePayDetails mobilePayDetails = (MobilePayDetails) o;
+    return Objects.equals(this.type, mobilePayDetails.type);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(type);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class MobilePayDetails {\n");
+
+    sb.append("    type: ").append(toIndentedString(type)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+
+}
