c76e9d067149bd6a537e4d16b1f25e408b6805fb
hafeez <hafeez.urrehman@adyen.com>
2020-10-15 17:01:43 +0200

    FOC-PW-3214 Add bacs payment method

---
+++ src/main/java/com/adyen/model/checkout/details/BacsDirectDebitDetails.java
@@ -0,0 +1,96 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Java API Library
+ *
+ * Copyright (c) 2020 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+
+package com.adyen.model.checkout.details;
+
+import java.util.Objects;
+
+import com.adyen.model.checkout.PaymentMethodDetails;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * BacsDirectDebitDetails
+ */
+public class BacsDirectDebitDetails implements PaymentMethodDetails {
+
+    public static final String DIRECTDEBIT_GB = "directdebit_GB";
+
+    @SerializedName("type")
+    private String type = DIRECTDEBIT_GB;
+
+    public BacsDirectDebitDetails type(String type) {
+        this.type = type;
+        return this;
+    }
+
+    /**
+     * **directdebit_GB**
+     *
+     * @return type
+     **/
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BacsDirectDebitDetails bacsDirectDebitDetails = (BacsDirectDebitDetails) o;
+        return Objects.equals(this.type, bacsDirectDebitDetails.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class BacsDirectDebitDetails {\n");
+
+        sb.append("    type: ").append(toIndentedString(type)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
+
+}
--- src/test/java/com/adyen/CheckoutTest.java
+++ src/test/java/com/adyen/CheckoutTest.java
@@ -799,6 +799,37 @@ public class CheckoutTest extends BaseTest {
     }
 
     @Test
+    public void TestBacsDirectDebitDetails() {
+        BacsDirectDebitDetails bacsDirectDebitDetails = new BacsDirectDebitDetails();
+
+        PaymentsRequest paymentsRequest = createBacsDirectDebitDetails();
+        paymentsRequest.setPaymentMethod(bacsDirectDebitDetails);
+
+        String jsonRequest = PRETTY_PRINT_GSON.toJson(paymentsRequest);
+
+        jsonRequest += "";
+
+        assertEquals("{\n"
+                + "  \"amount\": {\n"
+                + "    \"value\": 1000,\n"
+                + "    \"currency\": \"GBP\"\n"
+                + "  },\n"
+                + "  \"merchantAccount\": \"MagentoMerchantTest2\",\n"
+                + "  \"paymentMethod\": {\n"
+                + "    \"type\": \"directdebit_GB\"\n"
+                + "  },\n"
+                + "  \"reference\": \"Your order number\",\n"
+                + "  \"returnUrl\": \"https://your-company.com/...\",\n"
+                + "  \"applicationInfo\": {\n"
+                + "    \"adyenLibrary\": {\n"
+                + "      \"name\": \"adyen-java-api-library\",\n"
+                + "      \"version\": \"9.0.0\"\n"
+                + "    }\n"
+                + "  }\n"
+                + "}",jsonRequest);
+    }
+
+    @Test
     public void TestBilldeskWalletDetails(){
         BillDeskWalletDetails billdeskWalletDetails = new BillDeskWalletDetails();
         billdeskWalletDetails.setIssuer("111");
@@ -1645,6 +1676,21 @@ public class CheckoutTest extends BaseTest {
 
         paymentsRequest.setReturnUrl("https://your-company.com/...");
         paymentsRequest.setMerchantAccount("MagentoMerchantTest");
+
+        return paymentsRequest;
+    }
+
+    /**
+     * Returns a sample PaymentsRequest object with test data
+     */
+    protected PaymentsRequest createBacsDirectDebitDetails() {
+        PaymentsRequest paymentsRequest = new PaymentsRequest();
+
+        paymentsRequest.setReference("Your order number");
+        paymentsRequest.setAmount(createAmountObject("GBP", 1000L));
+
+        paymentsRequest.setReturnUrl("https://your-company.com/...");
+        paymentsRequest.setMerchantAccount("MagentoMerchantTest2");
 
         return paymentsRequest;
     }
