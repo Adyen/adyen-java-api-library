e7b9ba422687905fcba9d41efe3e6e7b665f6de8
alessio <alessio.zampatti@adyen.com>
2020-12-18 14:16:45 +0100

    add getter and setter for storedpaymentmethodid in PayPalDetails

--- src/test/java/com/adyen/CheckoutTest.java
+++ src/test/java/com/adyen/CheckoutTest.java
@@ -703,12 +703,13 @@ public class CheckoutTest extends BaseTest {
 
     @Test
     public void TestPayPalDetails() throws JsonProcessingException {
-        String expectedJson = "{\"amount\":{\"value\":1000,\"currency\":\"USD\"},\"merchantAccount\":\"MagentoMerchantTest\",\"paymentMethod\":{\"orderID\":\"orderId\",\"payerID\":\"payerId\",\"type\":\"paypal\",\"subtype\":\"sdk\"},\"reference\":\"Your order number\",\"returnUrl\":\"https://your-company.com/...\",\"applicationInfo\":{\"adyenLibrary\":{\"name\":\"adyen-java-api-library\",\"version\":\"" + LIB_VERSION + "\"}}}";
+        String expectedJson = "{\"amount\":{\"value\":1000,\"currency\":\"USD\"},\"merchantAccount\":\"MagentoMerchantTest\",\"paymentMethod\":{\"orderID\":\"orderId\",\"payerID\":\"payerId\",\"type\":\"paypal\",\"storedPaymentMethodId\":\"12345678\",\"subtype\":\"sdk\"},\"reference\":\"Your order number\",\"returnUrl\":\"https://your-company.com/...\",\"applicationInfo\":{\"adyenLibrary\":{\"name\":\"adyen-java-api-library\",\"version\":\"" + LIB_VERSION + "\"}}}";
 
         PayPalDetails payPalDetails = new PayPalDetails();
         payPalDetails.setOrderID("orderId");
         payPalDetails.setPayerID("payerId");
         payPalDetails.setSubtype(PayPalDetails.SubtypeEnum.SDK);
+        payPalDetails.setStoredPaymentMethodId("12345678");
         PaymentsRequest paymentsRequest = createPaymentsCheckoutRequest();
         paymentsRequest.setPaymentMethod(payPalDetails);
 
