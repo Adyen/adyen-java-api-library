ad5aebdac8329a34a19430816fc78e978ea79757
alessio <alessio.zampatti@adyen.com>
2021-05-03 11:04:09 +0200

    stylefix

--- src/main/java/com/adyen/model/marketpay/AccountEventContainer.java
+++ src/main/java/com/adyen/model/marketpay/AccountEventContainer.java
@@ -39,8 +39,12 @@ public class AccountEventContainer {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         AccountEventContainer that = (AccountEventContainer) o;
 
