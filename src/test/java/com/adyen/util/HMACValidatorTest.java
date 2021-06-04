4c80b757dbdeb2148da7280e8bbc72a974954e58
abhilash <abhilash.singh@adyen.com>
2021-06-04 12:08:10 +0200

    Removed unused import.

--- src/test/java/com/adyen/util/HMACValidatorTest.java
+++ src/test/java/com/adyen/util/HMACValidatorTest.java
@@ -29,7 +29,6 @@ import java.security.SignatureException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class HMACValidatorTest {
 
