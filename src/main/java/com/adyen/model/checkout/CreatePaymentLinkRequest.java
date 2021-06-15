f2fc43ae6d091f95d324f677ec8213ab0d81284e
saquib <saquib.sayyad@adyen.com>
2021-06-15 13:30:14 +0200

    PW-4930 Added copyright and change type

--- src/main/java/com/adyen/model/checkout/CreatePaymentLinkRequest.java
+++ src/main/java/com/adyen/model/checkout/CreatePaymentLinkRequest.java
@@ -14,7 +14,7 @@
  *
  * Adyen Java API Library
  *
- * Copyright (c) 2020 Adyen B.V.
+ * Copyright (c) 2021 Adyen B.V.
  * This file is open source and available under the MIT license.
  * See the LICENSE file for more info.
  */
@@ -130,7 +130,7 @@ public class CreatePaymentLinkRequest {
 
             @Override
             public RecurringProcessingModelEnum read(final JsonReader jsonReader) throws IOException {
-                Object value = jsonReader.nextString();
+                String value = jsonReader.nextString();
                 return RecurringProcessingModelEnum.fromValue(String.valueOf(value));
             }
         }
