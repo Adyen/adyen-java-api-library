b7cf6bf8616d86d219739f1b0bf5f3adf8c2b6ce
alessio <alessio.zampatti@adyen.com>
2021-06-28 16:07:12 +0200

    PW-4950: change @XmlAttribute to @XmlElement in Nexo models

--- src/main/java/com/adyen/model/nexo/CoinsOrBills.java
+++ src/main/java/com/adyen/model/nexo/CoinsOrBills.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 import java.math.BigDecimal;
 import java.math.BigInteger;
--- src/main/java/com/adyen/model/nexo/KEKIdentifier.java
+++ src/main/java/com/adyen/model/nexo/KEKIdentifier.java
@@ -2,6 +2,7 @@ package com.adyen.model.nexo;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlType;
 
 
