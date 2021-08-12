dde1fe07aac17342bf257993b4bfecbf79fb1cb5
Rick Wieman <rick.wieman@adyen.com>
2021-08-12 14:23:58 +0200

    Add "ultimateParentCompany*" in FieldType to follow OpenAPI spec

--- src/main/java/com/adyen/model/marketpay/FieldType.java
+++ src/main/java/com/adyen/model/marketpay/FieldType.java
@@ -25,7 +25,6 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.Objects;
 
-
 import static com.adyen.util.Util.toIndentedString;
 
 /**
@@ -194,6 +193,39 @@ public class FieldType {
 
         @SerializedName("tierNumber")
         TIERNUMBER("tierNumber"),
+
+        @SerializedName("ultimateParentCompany")
+        ULTIMATEPARENTCOMPANY("ultimateParentCompany"),
+
+        @SerializedName("ultimateParentCompanyAddressDetails")
+        ULTIMATEPARENTCOMPANYADDRESSDETAILS("ultimateParentCompanyAddressDetails"),
+
+        @SerializedName("ultimateParentCompanyAddressDetailsCountry")
+        ULTIMATEPARENTCOMPANYADDRESSDETAILSCOUNTRY("ultimateParentCompanyAddressDetailsCountry"),
+
+        @SerializedName("ultimateParentCompanyBusinessDetails")
+        ULTIMATEPARENTCOMPANYBUSINESSDETAILS("ultimateParentCompanyBusinessDetails"),
+
+        @SerializedName("ultimateParentCompanyBusinessDetailsLegalBusinessName")
+        ULTIMATEPARENTCOMPANYBUSINESSDETAILSLEGALBUSINESSNAME("ultimateParentCompanyBusinessDetailsLegalBusinessName"),
+
+        @SerializedName("ultimateParentCompanyBusinessDetailsRegistrationNumber")
+        ULTIMATEPARENTCOMPANYBUSINESSDETAILSREGISTRATIONNUMBER("ultimateParentCompanyBusinessDetailsRegistrationNumber"),
+
+        @SerializedName("ultimateParentCompanyCode")
+        ULTIMATEPARENTCOMPANYCODE("ultimateParentCompanyCode"),
+
+        @SerializedName("ultimateParentCompanyStockExchange")
+        ULTIMATEPARENTCOMPANYSTOCKEXCHANGE("ultimateParentCompanyStockExchange"),
+
+        @SerializedName("ultimateParentCompanyStockNumber")
+        ULTIMATEPARENTCOMPANYSTOCKNUMBER("ultimateParentCompanyStockNumber"),
+
+        @SerializedName("ultimateParentCompanyStockNumberOrStockTicker")
+        ULTIMATEPARENTCOMPANYSTOCKNUMBERORSTOCKTICKER("ultimateParentCompanyStockNumberOrStockTicker"),
+
+        @SerializedName("ultimateParentCompanyStockTicker")
+        ULTIMATEPARENTCOMPANYSTOCKTICKER("ultimateParentCompanyStockTicker"),
 
         @SerializedName("unknown")
         UNKNOWN("unknown"),
