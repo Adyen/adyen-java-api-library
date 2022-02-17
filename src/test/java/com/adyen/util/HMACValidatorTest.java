a6bcb82b0dabe07da222bb2292e7f4c2802fd3d4
Alexandros Moraitis <alexandros.moraitis@adyen.com>
2022-02-17 09:54:31 +0100

    Hmac validator null-safe check (#647)

--- src/test/java/com/adyen/util/HMACValidatorTest.java
+++ src/test/java/com/adyen/util/HMACValidatorTest.java
@@ -21,12 +21,10 @@
 
 package com.adyen.util;
 
+import java.security.SignatureException;
+import org.junit.Test;
 import com.adyen.model.notification.NotificationRequestItem;
 import com.google.gson.Gson;
-import org.junit.Test;
-
-import java.security.SignatureException;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
@@ -57,6 +55,59 @@ public class HMACValidatorTest {
         NotificationRequestItem notificationRequest = new Gson().fromJson(notificationJson, NotificationRequestItem.class);
         boolean result = new HMACValidator().validateHMAC(notificationRequest, HMAC_KEY);
         assertTrue(result);
+    }
+
+    @Test
+    public void testValidateHMACNullHmacSignature() throws SignatureException {
+        String notificationJson = "{\n"
+                + "    \"additionalData\": {\n"
+                + "    },\n"
+                + "    \"amount\": {\n"
+                + "        \"currency\": \"EUR\",\n"
+                + "        \"value\": \"0\"\n"
+                + "    },\n"
+                + "    \"eventCode\": \"REPORT_AVAILABLE\",\n"
+                + "    \"eventDate\": \"2019-11-20T14:35:36+01:00\",\n"
+                + "    \"merchantAccountCode\": \"Magento2Rik\",\n"
+                + "    \"merchantReference\": \"testMerchantRef1\",\n"
+                + "    \"pspReference\": \"test_REPORT_AVAILABLE\",\n"
+                + "    \"reason\": \"will contain the url to the report\",\n"
+                + "    \"success\": \"true\"\n"
+                + "}";
+        try {
+            NotificationRequestItem notificationRequest = new Gson().fromJson(notificationJson, NotificationRequestItem.class);
+            boolean result = new HMACValidator().validateHMAC(notificationRequest, HMAC_KEY);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Missing hmacSignature", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testValidateHMACEmptyHmacSignature() throws SignatureException {
+        String notificationJson = "{\n"
+                + "    \"additionalData\": {\n"
+                + "        \"hmacSignature\": \"\"\n"
+                + "    },\n"
+                + "    \"amount\": {\n"
+                + "        \"currency\": \"EUR\",\n"
+                + "        \"value\": \"0\"\n"
+                + "    },\n"
+                + "    \"eventCode\": \"REPORT_AVAILABLE\",\n"
+                + "    \"eventDate\": \"2019-11-20T14:35:36+01:00\",\n"
+                + "    \"merchantAccountCode\": \"Magento2Rik\",\n"
+                + "    \"merchantReference\": \"testMerchantRef1\",\n"
+                + "    \"pspReference\": \"test_REPORT_AVAILABLE\",\n"
+                + "    \"reason\": \"will contain the url to the report\",\n"
+                + "    \"success\": \"true\"\n"
+                + "}";
+        try {
+            NotificationRequestItem notificationRequest = new Gson().fromJson(notificationJson, NotificationRequestItem.class);
+            boolean result = new HMACValidator().validateHMAC(notificationRequest, HMAC_KEY);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertEquals("Missing hmacSignature", e.getMessage());
+        }
     }
 
     @Test
