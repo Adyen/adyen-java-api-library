b703ba0f34fa92b095d4ac83d3dcff51354a1671
alexandros <alexandros.moraitis@adyen.com>
2022-05-03 11:32:59 +0200

    Pump the version

--- README.md
+++ README.md
@@ -39,7 +39,7 @@ You can use Maven and add this dependency to your project's POM:
 <dependency>
   <groupId>com.adyen</groupId>
   <artifactId>adyen-java-api-library</artifactId>
-  <version>17.3.0</version>
+  <version>17.4.0</version>
 </dependency>
 ```
 
--- src/main/java/com/adyen/Client.java
+++ src/main/java/com/adyen/Client.java
@@ -47,7 +47,7 @@ public class Client {
     public static final String MARKETPAY_NOTIFICATION_API_VERSION = "v6";
     public static final String MARKETPAY_HOP_API_VERSION = "v6";
     public static final String LIB_NAME = "adyen-java-api-library";
-    public static final String LIB_VERSION = "17.3.0";
+    public static final String LIB_VERSION = "17.4.0";
     public static final String CHECKOUT_ENDPOINT_TEST = "https://checkout-test.adyen.com/checkout";
     public static final String CHECKOUT_ENDPOINT_LIVE_SUFFIX = "-checkout-live.adyenpayments.com/checkout";
     public static final String CHECKOUT_ENDPOINT_CERT_LIVE = "https://checkoutcert-live-%s.adyen.com/checkout";
