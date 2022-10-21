da8b5ad3e4775e439a8f9f14eb7d3b75e91ab414
alexandros <alexandros.moraitis@adyen.com>
2022-10-21 14:57:01 +0200

    Add Bank and Issuing Fund Transfers V3 API

---
+++ src/main/java/com/adyen/service/Transfers.java
@@ -0,0 +1,26 @@
+package com.adyen.service;
+
+import com.adyen.Client;
+import com.adyen.Service;
+import com.adyen.model.transfer.JSON;
+import com.adyen.model.transfer.Transfer;
+import com.adyen.model.transfer.TransferInfo;
+import com.adyen.service.exception.ApiException;
+import com.adyen.service.resource.transfers.TransfersResource;
+
+import java.io.IOException;
+
+public class Transfers extends Service {
+
+    public Transfers(Client client) {
+        super(client);
+        new JSON();
+    }
+
+    public Transfer transfers(TransferInfo transferInfo) throws IOException, ApiException {
+        String jsonRequest = GSON.toJson(transferInfo);
+        TransfersResource transfersResource = new TransfersResource(this, "/transfers");
+        String jsonResult = transfersResource.request(jsonRequest);
+        return Transfer.fromJson(jsonResult);
+    }
+}
