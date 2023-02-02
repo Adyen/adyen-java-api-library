1234de9846b48995faea4275951ed18c0a917e2a
Wouter Boereboom <62436079+wboereboom@users.noreply.github.com>
2023-02-02 15:02:12 +0100

    fix transfers models directory and reference (#954)

--- src/main/java/com/adyen/service/Transfers.java
+++ src/main/java/com/adyen/service/Transfers.java
@@ -3,12 +3,12 @@ package com.adyen.service;
 import com.adyen.Client;
 import com.adyen.Service;
 import com.adyen.constants.ApiConstants;
-import com.adyen.model.transfer.JSON;
+import com.adyen.model.transfers.JSON;
 
-import com.adyen.model.transfer.TransactionSearchResponse;
-import com.adyen.model.transfer.Transfer;
-import com.adyen.model.transfer.TransferInfo;
-import com.adyen.model.transfer.Transaction;
+import com.adyen.model.transfers.TransactionSearchResponse;
+import com.adyen.model.transfers.Transfer;
+import com.adyen.model.transfers.TransferInfo;
+import com.adyen.model.transfers.Transaction;
 import com.adyen.service.exception.ApiException;
 import com.adyen.service.resource.transfers.TransfersResource;
 
