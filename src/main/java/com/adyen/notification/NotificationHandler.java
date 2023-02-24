53dbe1f596eddc84ab4199a4767b7d01aec9f8b0
jillingk <93914435+jillingk@users.noreply.github.com>
2023-02-24 14:08:25 +0100

    created notification handler for Terminal event/display notifications

--- src/main/java/com/adyen/notification/NotificationHandler.java
+++ src/main/java/com/adyen/notification/NotificationHandler.java
@@ -23,6 +23,8 @@ package com.adyen.notification;
 import com.adyen.deserializer.MarketPayNotificationMessageDeserializer;
 import com.adyen.model.marketpay.notification.GenericNotification;
 import com.adyen.model.notification.NotificationRequest;
+import com.adyen.model.terminal.TerminalAPIRequest;
+import com.adyen.terminal.serialization.TerminalAPIGsonBuilder;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -33,11 +35,13 @@ import com.google.gson.reflect.TypeToken;
 public class NotificationHandler {
     private static final Gson GSON = new Gson();
     private final Gson marketPayGson;
+    private final Gson terminalGson;
 
     public NotificationHandler() {
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.registerTypeAdapter(GenericNotification.class, new MarketPayNotificationMessageDeserializer());
         marketPayGson = gsonBuilder.create();
+        terminalGson = TerminalAPIGsonBuilder.create();
     }
 
     public NotificationRequest handleNotificationJson(String json) {
@@ -47,5 +51,11 @@ public class NotificationHandler {
 
     public GenericNotification handleMarketpayNotificationJson(String json) {
         return marketPayGson.fromJson(json, GenericNotification.class);
+    }
+
+    // Note that terminal notifications are structured as TerminalAPIRequest objects
+    public TerminalAPIRequest handleTerminalNotificationJson(String json){
+        return terminalGson.fromJson(json, new TypeToken<TerminalAPIRequest>() {
+        }.getType());
     }
 }
