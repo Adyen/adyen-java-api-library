fb4d34dfd693cc79080945f6c5dc285a667c8118
jillingk <93914435+jillingk@users.noreply.github.com>
2023-05-26 15:17:35 +0100

    new field in requestOptions for additional headers (#1039)

--- src/main/java/com/adyen/model/RequestOptions.java
+++ src/main/java/com/adyen/model/RequestOptions.java
@@ -1,9 +1,12 @@
 package com.adyen.model;
 
+import java.util.HashMap;
+
 public class RequestOptions {
 
     private String idempotencyKey;
     private String requestedVerificationCodeHeader;
+    private HashMap<String, String> additionalServiceHeaders;
 
     public String getIdempotencyKey() {
         return idempotencyKey;
@@ -19,6 +22,14 @@ public class RequestOptions {
 
     public void setRequestedVerificationCodeHeader(String requestedVerificationCodeHeader) {
         this.requestedVerificationCodeHeader = requestedVerificationCodeHeader;
+    }
+
+    public HashMap<String, String> getAdditionalServiceHeaders() {
+        return additionalServiceHeaders;
+    }
+
+    public void setAdditionalServiceHeaders(HashMap<String, String> additionalServiceHeaders) {
+        this.additionalServiceHeaders = additionalServiceHeaders;
     }
 
 }
