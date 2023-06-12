557b387a7080dfba3413e5b09af704c1681f91e1
Antoni Stroinski <55943882+antolo-arch@users.noreply.github.com>
2023-06-12 10:17:58 +0200

    try new approach

--- .github/scripts/release.js
+++ .github/scripts/release.js
@@ -35,28 +35,6 @@ exports.updatePythonVersion = async (version) => {
   fs.writeFileSync('setup.py', newVersion, 'utf-8');
 }
 
-// Get the current version in pom.xml file
-
-exports.pomJavaVersion = () => {
-    const fs = require('fs');
-    const re = /<version>(\d{1,2}.\d.\d)<\/version>\n.*<name>Adyen Java API Library<\/name>/;
-    data = fs.readFileSync("pom.xml", 'utf-8');
-    version = data.match(re)[1];
-    return version;
-}
-
-// Update version in pom.xml and settings
-exports.updateJavaVersion = async (version) => {
-  const fs = require('fs');
-  data = fs.readFileSync('src/main/java/com/adyen/Client.java', 'utf-8');
-  newVersion = data.replace(/(LIB_VERSION = )"(\d{1,2}\.\d\.\d)"/, "$1" + '"' + version +'"');
-  fs.writeFileSync('src/main/java/com/adyen/Client.java', newVersion, 'utf-8');
-
-  data = fs.readFileSync('pom.xml', 'utf-8');
-  newVersion = data.replace(/(<version>)(\d{1,2}.\d.\d)(<\/version>\n.*<name>Adyen Java API Library<\/name>)/, "$1" + version + "$3");
-  fs.writeFileSync('pom.xml', newVersion, 'utf-8');
-}
-
 // Next semantic version number
 exports.nextVersion = (current, increment) => {
   let [major, minor, patch] = current.split('.');
