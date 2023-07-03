5f870f4df70910a79a2cb35a46f1778571e96980
jillingk <93914435+jillingk@users.noreply.github.com>
2023-07-03 16:52:02 +0200

    Refactored Banking Webhooks to use Jackson (#1076)

--- src/main/java/com/adyen/notification/BankingWebhookHandler.java
+++ src/main/java/com/adyen/notification/BankingWebhookHandler.java
@@ -6,20 +6,14 @@ import com.adyen.model.configurationwebhooks.CardOrderNotificationRequest;
 import com.adyen.model.configurationwebhooks.PaymentNotificationRequest;
 import com.adyen.model.configurationwebhooks.SweepConfigurationNotificationRequest;
 import com.adyen.model.reportwebhooks.ReportNotificationRequest;
+import com.adyen.model.transferwebhooks.JSON;
 import com.adyen.model.transferwebhooks.TransferNotificationRequest;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import java.util.Optional;
 
 public class BankingWebhookHandler {
-    private static final Gson GSON = new Gson();
-    private final Gson bankingGson;
-    private String payload;
+    private final String payload;
 
     public BankingWebhookHandler(String payload) {
-
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        this.bankingGson = gsonBuilder.create();
         this.payload = payload;
     }
 
@@ -53,7 +47,7 @@ public class BankingWebhookHandler {
 
     private <T> Optional<T> getOptionalField(Class<T> clazz) {
         try {
-            T val = bankingGson.fromJson(this.payload, clazz);
+            T val = JSON.getMapper().readValue(payload, clazz);
             return Optional.ofNullable(val);
         } catch (Exception e) {
             return Optional.empty();
