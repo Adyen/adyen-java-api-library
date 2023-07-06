140f4d068f27381683a548bab6428e5082898653
Wouter Boereboom <62436079+wboereboom@users.noreply.github.com>
2023-07-06 15:18:00 +0200

    rename Transfers service to TransfersApi and move capital endpoints into TransfersApi (#1072)

--- src/test/java/com/adyen/TransfersTest.java
+++ src/test/java/com/adyen/TransfersTest.java
@@ -1,18 +1,23 @@
 package com.adyen;
 
+import com.adyen.constants.ApiConstants;
 import com.adyen.model.transfers.*;
-import com.adyen.service.Transfers;
+import com.adyen.service.transfers.*;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+
+import java.util.HashMap;
 
 public class TransfersTest extends BaseTest {
 
     @Test
     public void TransferTest() throws Exception {
         Client client = createMockClientFromFile("mocks/transfers/post-transfers-payout-cross-border-200.json");
-        Transfers transfers = new Transfers(client);
-        Transfer response = transfers.transfers(new TransferInfo());
+        TransfersApi transfers = new TransfersApi(client);
+        Transfer response = transfers.transferFunds(new TransferInfo());
         Amount amount = new Amount();
         amount.setCurrency("EUR");
         amount.setValue(80000L);
@@ -24,8 +29,8 @@ public class TransfersTest extends BaseTest {
     @Test
     public void TransactionsListTest() throws Exception {
         Client client = createMockClientFromFile("mocks/transfers/get-transactions-success-200.json");
-        Transfers transactions = new Transfers(client);
-        TransactionSearchResponse response = transactions.listTransactions();
+        TransactionsApi transactions = new TransactionsApi(client);
+        TransactionSearchResponse response = transactions.getAllTransactions(null, null);
         Transaction transactionsResponse = response.getData().get(1);
         assertEquals(transactionsResponse.getAccountHolderId(), "AHA1B2C3D4E5F6G7H8I9J0");
         assertEquals(transactionsResponse.getCounterparty().getBalanceAccountId(), "BAB8B2C3D4E5F6G7H8D9J6GD4");
@@ -35,10 +40,44 @@ public class TransfersTest extends BaseTest {
     @Test
     public void TransactionsRetrieveTest() throws Exception {
         Client client = createMockClientFromFile("mocks/transfers/get-transactions-id-success-200.json");
-        Transfers transactions = new Transfers(client);
+        TransactionsApi transactions = new TransactionsApi(client);
         Transaction response = transactions.getTransaction("1");
         assertEquals(response.getAccountHolderId(), "AHA1B2C3D4E5F6G7H8I9J0");
         assertEquals(response.getBalancePlatform(), "YOUR_BALANCE_PLATFORM");
         assertEquals(response.getTransferId(), "2QP32A5U7IWC5WKG");
+    }
+
+     @Test
+    public void TestGetCapitalAccounts() throws Exception {
+        Client client = createMockClientFromFile("mocks/capital/get-capital-account.json");
+        CapitalApi capital = new CapitalApi(client);
+        CapitalGrants response = capital.getCapitalAccount();
+        assertEquals(response.getGrants().get(0).getGrantOfferId(), "string");
+        verify(client.getHttpClient()).request(
+                "https://balanceplatform-api-test.adyen.com/btl/v3/grants",
+                null,
+                client.getConfig(),
+                false,
+                null,
+                ApiConstants.HttpMethod.GET,
+                new HashMap<>()
+        );
+    }
+
+    @Test
+    public void TestRequestGrant() throws Exception {
+        Client client = createMockClientFromFile("mocks/capital/request-grant.json");
+        CapitalApi capital = new CapitalApi(client);
+        CapitalGrant response = capital.requestGrantPayout(new CapitalGrantInfo());
+        assertEquals(response.getGrantAccountId(), "CG00000000000000000000001");
+        verify(client.getHttpClient()).request(
+                "https://balanceplatform-api-test.adyen.com/btl/v3/grants",
+                "{}",
+                client.getConfig(),
+                false,
+                null,
+                ApiConstants.HttpMethod.POST,
+                null
+        );
     }
 }
