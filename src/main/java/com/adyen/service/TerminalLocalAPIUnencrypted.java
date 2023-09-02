ec4e1119c5b2ab321bc559172b34a389e277690c
alessio <alessio.zampatti@adyen.com>
2023-09-02 11:53:46 +0200

    Add Terminal API local unencrypted class

---
+++ src/main/java/com/adyen/service/TerminalLocalAPIUnencrypted.java
@@ -0,0 +1,70 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Java API Library
+ *
+ * Copyright (c) 2019 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
+package com.adyen.service;
+
+import com.adyen.ApiKeyAuthenticatedService;
+import com.adyen.Client;
+import com.adyen.model.terminal.TerminalAPIRequest;
+import com.adyen.model.terminal.TerminalAPIResponse;
+import com.adyen.service.resource.terminal.local.LocalRequest;
+import com.adyen.terminal.serialization.TerminalAPIGsonBuilder;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+/**
+ * [UNENCRYPTED] Local Terminal Api.
+ * Use this class (in TEST only) to experiment with the Local Terminal API separately
+ * from the encryption implementation required for live payments.
+ * <p>
+ * Be sure to remove the encryption key details on the Customer Area as it will not work with encryption key details set up.
+ */
+public class TerminalLocalAPIUnencrypted extends ApiKeyAuthenticatedService {
+
+    private final LocalRequest localRequest;
+
+    private final Gson terminalApiGson;
+
+    public TerminalLocalAPIUnencrypted(Client client) {
+        super(client);
+        localRequest = new LocalRequest(this);
+        terminalApiGson = TerminalAPIGsonBuilder.create();
+    }
+
+    /**
+     * Local Terminal API call
+     *
+     * @param terminalAPIRequest TerminalAPIRequest
+     * @return TerminalAPIResponse
+     * @throws Exception exception
+     */
+    public TerminalAPIResponse request(TerminalAPIRequest terminalAPIRequest) throws Exception {
+        String jsonRequest = terminalApiGson.toJson(terminalAPIRequest);
+
+        String jsonResponse = localRequest.request(jsonRequest);
+
+        if (jsonResponse == null || jsonResponse.isEmpty() || "ok".equals(jsonResponse)) {
+            return null;
+        }
+
+        return terminalApiGson.fromJson(jsonResponse, new TypeToken<TerminalAPIResponse>() {
+        }.getType());
+    }
+}
