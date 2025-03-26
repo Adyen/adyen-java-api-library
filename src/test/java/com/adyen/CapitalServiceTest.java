87fd2d0cebc64b8c2e52920597363b91132c8bb1
gcatanese <gcatanese@yahoo.com>
2025-03-27 00:28:02 +0100

    Add unit tests

---
+++ src/test/java/com/adyen/CapitalServiceTest.java
@@ -0,0 +1,60 @@
+package com.adyen;
+
+import com.adyen.model.binlookup.ThreeDSAvailabilityRequest;
+import com.adyen.model.binlookup.ThreeDSAvailabilityResponse;
+import com.adyen.model.capital.*;
+import com.adyen.service.BinLookupApi;
+import com.adyen.service.capital.GrantsApi;
+import com.adyen.service.transfers.CapitalApi;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CapitalServiceTest extends BaseTest {
+
+    @Test
+    public void testGetAllGrants() throws Exception {
+
+        Client client = createMockClientFromFile("mocks/capital/get-grants-200.json");
+        GrantsApi grantsApi = new GrantsApi(client);
+
+        Grants grants = grantsApi.getAllGrants("AH000000001");
+        assertNotNull(grants);
+        assertEquals(1, grants.getGrants().size());
+
+    }
+
+    @Test
+    public void testGetGrantDisbursement() throws Exception {
+
+        Client client = createMockClientFromFile("mocks/capital/get-grants-disbursement-200.json");
+        GrantsApi grantsApi = new GrantsApi(client);
+
+        PatchableDisbursement patchableDisbursement = new PatchableDisbursement()
+                .repayment(new PatchableDisbursementRepayment()
+                        .basisPoints(100));
+
+        Disbursement disbursement = grantsApi.updateGrantDisbursement("0000001", "0000002", patchableDisbursement);
+        assertNotNull(disbursement);
+        assertEquals(1, disbursement.getFundsCollections().size());
+    }
+
+    @Test
+    public void testRequestGrant() throws Exception {
+
+        Client client = createMockClientFromFile("mocks/capital/request-grant-200.json");
+        GrantsApi grantsApi = new GrantsApi(client);
+
+        GrantInfo grantInfo = new GrantInfo()
+                .grantAccountId("AH000000001")
+                .grantOfferId("AH000000001")
+                .amount(new Amount()
+                        .currency("EUR")
+                        .value(1000L));
+
+        Grant grant = grantsApi.requestGrant(grantInfo);
+        assertNotNull(grant);
+        assertEquals("GR00000000000000000000001", grant.getId());
+    }
+
+}
