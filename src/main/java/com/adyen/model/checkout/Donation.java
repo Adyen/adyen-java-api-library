36086401848efe241a75042544b960ecbbb7dbb6
gcatanese <gcatanese@yahoo.com>
2025-04-29 16:25:26 +0200

    Remove new Donation type from patch

--- src/main/java/com/adyen/model/checkout/Donation.java
+++ src/main/java/com/adyen/model/checkout/Donation.java
@@ -34,7 +34,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
   Donation.JSON_PROPERTY_CURRENCY,
   Donation.JSON_PROPERTY_DONATION_TYPE,
   Donation.JSON_PROPERTY_MAX_ROUNDUP_AMOUNT,
-  Donation.JSON_PROPERTY_TYPE,
   Donation.JSON_PROPERTY_VALUES
 })
 
@@ -47,9 +46,6 @@ public class Donation {
 
   public static final String JSON_PROPERTY_MAX_ROUNDUP_AMOUNT = "maxRoundupAmount";
   private Long maxRoundupAmount;
-
-  public static final String JSON_PROPERTY_TYPE = "type";
-  private String type;
 
   public static final String JSON_PROPERTY_VALUES = "values";
   private List<Long> values;
@@ -154,38 +150,6 @@ public class Donation {
   }
 
   /**
-   * The [type of donation](https://docs.adyen.com/online-payments/donations/#donation-types).  Possible values: * **roundup**: a donation where the original transaction amount is rounded up as a donation. * **fixedAmounts**: a donation where you show fixed donation amounts that the shopper can select from.
-   *
-   * @param type
-   * @return the current {@code Donation} instance, allowing for method chaining
-   */
-  public Donation type(String type) {
-    this.type = type;
-    return this;
-  }
-
-  /**
-   * The [type of donation](https://docs.adyen.com/online-payments/donations/#donation-types).  Possible values: * **roundup**: a donation where the original transaction amount is rounded up as a donation. * **fixedAmounts**: a donation where you show fixed donation amounts that the shopper can select from.
-   * @return type
-   */
-  @JsonProperty(JSON_PROPERTY_TYPE)
-  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
-  public String getType() {
-    return type;
-  }
-
-  /**
-   * The [type of donation](https://docs.adyen.com/online-payments/donations/#donation-types).  Possible values: * **roundup**: a donation where the original transaction amount is rounded up as a donation. * **fixedAmounts**: a donation where you show fixed donation amounts that the shopper can select from.
-   *
-   * @param type
-   */
-  @JsonProperty(JSON_PROPERTY_TYPE)
-  @JsonInclude(value = JsonInclude.Include.USE_DEFAULTS)
-  public void setType(String type) {
-    this.type = type;
-  }
-
-  /**
    * The fixed donation amounts in [minor units](https://docs.adyen.com/development-resources/currency-codes//#minor-units). This field is only present when &#x60;donationType&#x60; is **fixedAmounts**.
    *
    * @param values
@@ -240,13 +204,12 @@ public class Donation {
     return Objects.equals(this.currency, donation.currency) &&
         Objects.equals(this.donationType, donation.donationType) &&
         Objects.equals(this.maxRoundupAmount, donation.maxRoundupAmount) &&
-        Objects.equals(this.type, donation.type) &&
         Objects.equals(this.values, donation.values);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(currency, donationType, maxRoundupAmount, type, values);
+    return Objects.hash(currency, donationType, maxRoundupAmount, values);
   }
 
   @Override
@@ -256,7 +219,6 @@ public class Donation {
     sb.append("    currency: ").append(toIndentedString(currency)).append("\n");
     sb.append("    donationType: ").append(toIndentedString(donationType)).append("\n");
     sb.append("    maxRoundupAmount: ").append(toIndentedString(maxRoundupAmount)).append("\n");
-    sb.append("    type: ").append(toIndentedString(type)).append("\n");
     sb.append("    values: ").append(toIndentedString(values)).append("\n");
     sb.append("}");
     return sb.toString();
