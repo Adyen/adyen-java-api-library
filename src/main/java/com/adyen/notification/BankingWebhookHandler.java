fdaeed16d47aa9bc26a02cd46a45099d38ecf8bc
gcatanese <gcatanese@yahoo.com>
2025-05-01 09:32:20 +0200

    Deprecate former WebhookHandler implementation

--- src/main/java/com/adyen/notification/BankingWebhookHandler.java
+++ src/main/java/com/adyen/notification/BankingWebhookHandler.java
@@ -15,54 +15,76 @@ import com.adyen.model.transactionwebhooks.TransactionNotificationRequestV4;
 import java.util.Optional;
 import java.util.logging.Logger;
 
+/**
+ * DEPRECATED
+ *
+ * Centralised handler for de-serialising all (Banking) webhook payloads.
+ *
+ * @deprecated Each webhook provides its own handler, for example use AcsWebhooksHandler to process AcsWebhooks events
+ * Use the specific WebhookHandler implementation: AcsWebhooks, ReportWebhooksHandler, ConfigurationWebhooks,
+ * TransferWebhooks, TransactionWebhooks, etc..
+ */
+@Deprecated (since = "v37.0.0", forRemoval = true)
 public class BankingWebhookHandler {
 
     private static final Logger LOG = Logger.getLogger(BankingWebhookHandler.class.getName());
 
     private final String payload;
 
+    @Deprecated (since = "v37.0.0", forRemoval = true)
     public BankingWebhookHandler(String payload) {
         this.payload = payload;
     }
 
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use AcsWebhooksHandler instead
     public Optional<AuthenticationNotificationRequest> getAuthenticationNotificationRequest() {
         return getOptionalField(AuthenticationNotificationRequest.class);
     }
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use AcsWebhooksHandler instead
     public Optional<RelayedAuthenticationRequest> getRelayedAuthenticationRequest() {
         return getOptionalField(RelayedAuthenticationRequest.class);
     }
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use ConfigurationWebhooksHandler instead
     public Optional<AccountHolderNotificationRequest> getAccountHolderNotificationRequest() {
         return getOptionalField(AccountHolderNotificationRequest.class);
     }
 
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use ConfigurationWebhooksHandler instead
     public Optional<BalanceAccountNotificationRequest> getBalanceAccountNotificationRequest() {
         return getOptionalField(BalanceAccountNotificationRequest.class);
     }
 
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use ConfigurationWebhooksHandler instead
     public Optional<CardOrderNotificationRequest> getCardOrderNotificationRequest() {
         return getOptionalField(CardOrderNotificationRequest.class);
     }
 
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use ConfigurationWebhooksHandler instead
     public Optional<PaymentNotificationRequest> getPaymentNotificationRequest() {
         return getOptionalField(PaymentNotificationRequest.class);
     }
 
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use ConfigurationWebhooksHandler instead
     public Optional<SweepConfigurationNotificationRequest> getSweepConfigurationNotificationRequest() {
         return getOptionalField(SweepConfigurationNotificationRequest.class);
     }
 
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use ReportWebhooksHandler instead
     public Optional<ReportNotificationRequest> getReportNotificationRequest() {
         return getOptionalField(ReportNotificationRequest.class);
     }
 
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use TransferWebhooksHandler instead
     public Optional<TransferNotificationRequest> getTransferNotificationRequest() {
         return getOptionalField(TransferNotificationRequest.class);
     }
 
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use TransactionWebhooksHandler instead
     public Optional<TransactionNotificationRequestV4> getTransactionNotificationRequest() {
         return getOptionalField(TransactionNotificationRequestV4.class);
     }
 
+    @Deprecated (since = "v37.0.0", forRemoval = true) // use NegativeBalanceWarningWebhooksHandler instead
     public Optional<NegativeBalanceCompensationWarningNotificationRequest> getNegativeBalanceCompensationWarningNotificationRequest() {
         return getOptionalField(NegativeBalanceCompensationWarningNotificationRequest.class);
     }
