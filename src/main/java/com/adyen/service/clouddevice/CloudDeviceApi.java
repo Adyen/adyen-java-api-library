1ffc7d3d72929d92cf1bc0b3bdb95fb6f2bd92a0
gcatanese <gcatanese@yahoo.com>
2025-09-24 16:10:46 +0200

    Implement all endpoints and related tests

--- src/main/java/com/adyen/service/clouddevice/CloudDeviceApi.java
+++ src/main/java/com/adyen/service/clouddevice/CloudDeviceApi.java
@@ -3,56 +3,181 @@ package com.adyen.service.clouddevice;
 import com.adyen.Client;
 import com.adyen.Service;
 import com.adyen.constants.ApiConstants;
-import com.adyen.model.binlookup.ThreeDSAvailabilityResponse;
-import com.adyen.model.clouddevice.CloudDeviceApiRequest;
-import com.adyen.model.clouddevice.CloudDeviceApiResponse;
+import com.adyen.model.clouddevice.*;
 import com.adyen.service.resource.Resource;
-
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CloudDeviceApi extends Service {
 
-	public static final String API_VERSION = "1";
+  public static final String API_VERSION = "1";
 
-	protected String baseURL;
+  protected String baseURL;
 
-	/**
-	 * CloudDeviceApi constructor in {@link com.adyen.service.clouddevice package}.
-	 *
-	 * @param client {@link Client } (required)
-	 */
-	public CloudDeviceApi(Client client) {
-		super(client);
-		this.baseURL = createBaseURL("");
-	}
+  /**
+   * CloudDeviceApi constructor in {@link com.adyen.service.clouddevice package}.
+   *
+   * @param client {@link Client } (required)
+   */
+  public CloudDeviceApi(Client client) {
+    super(client);
+    this.baseURL = createBaseURL("https://device-api-test.adyen.com/v1");
+  }
 
-	public CloudDeviceApiResponse sendSync(String merchantAccount, String deviceId, CloudDeviceApiRequest cloudDeviceApiRequest) throws Exception {
+  /**
+   * Send a synchronous payment request.
+   *
+   * @param merchantAccount The unique identifier of the merchant account
+   * @param deviceId The unique identifier of the payment device that you send this request to (must
+   *     match POIID in the MessageHeader).
+   * @param cloudDeviceApiRequest The request to send.
+   * @return instance of CloudDeviceApiResponse
+   * @throws Exception when an error occurs
+   */
+  public CloudDeviceApiResponse sendSync(
+      String merchantAccount, String deviceId, CloudDeviceApiRequest cloudDeviceApiRequest)
+      throws Exception {
 
-		// Add path params
-		Map<String, String> pathParams = new HashMap<>();
+    // Add path params
+    Map<String, String> pathParams = new HashMap<>();
 
-		if (merchantAccount == null) {
-			throw new IllegalArgumentException("Please provide the merchantAccount path parameter");
-		}
-		pathParams.put("merchantAccount", merchantAccount);
+    if (merchantAccount == null) {
+      throw new IllegalArgumentException("Please provide the merchantAccount path parameter");
+    }
+    pathParams.put("merchantAccount", merchantAccount);
 
-		if (deviceId == null) {
-			throw new IllegalArgumentException("Please provide the deviceId path parameter");
-		}
-		pathParams.put("deviceId", deviceId);
+    if (deviceId == null) {
+      throw new IllegalArgumentException("Please provide the deviceId path parameter");
+    }
+    pathParams.put("deviceId", deviceId);
 
-		// set deviceId
-		cloudDeviceApiRequest.getSaleToPOIRequest().getMessageHeader().setPOIID(deviceId);
+    // set deviceId
+    cloudDeviceApiRequest.getSaleToPOIRequest().getMessageHeader().setPOIID(deviceId);
 
-		String requestBody = cloudDeviceApiRequest.toJson();
-		Resource resource = new Resource(this, this.baseURL + "/merchants/{merchantAccount}/devices/{deviceId}/sync", null);
-		String jsonResult = resource.request(requestBody, null, ApiConstants.HttpMethod.POST, pathParams);
+    String requestBody = cloudDeviceApiRequest.toJson();
+    Resource resource =
+        new Resource(
+            this, this.baseURL + "/merchants/{merchantAccount}/devices/{deviceId}/sync", null);
+    String response = resource.request(requestBody, null, ApiConstants.HttpMethod.POST, pathParams);
 
-		return CloudDeviceApiResponse.fromJson(jsonResult);
+    return CloudDeviceApiResponse.fromJson(response);
+  }
 
-	}
+  /**
+   * Send an asynchronous payment request.
+   *
+   * @param merchantAccount The unique identifier of the merchant account
+   * @param deviceId The unique identifier of the device that you send this request to (must match
+   *     POIID in the MessageHeader).
+   * @param cloudDeviceApiRequest The request to send.
+   * @return instance of CloudDeviceApiResponse
+   * @throws Exception when an error occurs
+   */
+  public String sendAsync(
+      String merchantAccount, String deviceId, CloudDeviceApiRequest cloudDeviceApiRequest)
+      throws Exception {
 
+    // Add path params
+    Map<String, String> pathParams = new HashMap<>();
 
+    if (merchantAccount == null) {
+      throw new IllegalArgumentException("Please provide the merchantAccount path parameter");
+    }
+    pathParams.put("merchantAccount", merchantAccount);
+
+    if (deviceId == null) {
+      throw new IllegalArgumentException("Please provide the deviceId path parameter");
+    }
+    pathParams.put("deviceId", deviceId);
+
+    // set deviceId
+    cloudDeviceApiRequest.getSaleToPOIRequest().getMessageHeader().setPOIID(deviceId);
+
+    String requestBody = cloudDeviceApiRequest.toJson();
+    Resource resource =
+        new Resource(
+            this, this.baseURL + "/merchants/{merchantAccount}/devices/{deviceId}/async", null);
+    String response = resource.request(requestBody, null, ApiConstants.HttpMethod.POST, pathParams);
+
+    return response;
+  }
+
+  /**
+   * Get a list of connected devices for a merchant account.
+   *
+   * @param merchantAccount The unique identifier of the merchant account.
+   * @return An instance of ConnectedDevicesResponse.
+   * @throws Exception when an error occurs
+   */
+  public ConnectedDevicesResponse getConnectedDevices(String merchantAccount) throws Exception {
+    return getConnectedDevices(merchantAccount, null);
+  }
+
+  /**
+   * Get a list of connected devices for a merchant account and store.
+   *
+   * @param merchantAccount The unique identifier of the merchant account.
+   * @param store The unique identifier of the store.
+   * @return An instance of ConnectedDevicesResponse.
+   * @throws Exception when an error occurs
+   */
+  public ConnectedDevicesResponse getConnectedDevices(String merchantAccount, String store)
+      throws Exception {
+    // Add path params
+    Map<String, String> pathParams = new HashMap<>();
+
+    if (merchantAccount == null) {
+      throw new IllegalArgumentException("Please provide the merchantAccount path parameter");
+    }
+    pathParams.put("merchantAccount", merchantAccount);
+
+    // Add query params
+    Map<String, String> queryParams = new HashMap<>();
+    if (store != null) {
+      queryParams.put("store", store);
+    }
+
+    Resource resource =
+        new Resource(this, this.baseURL + "/merchants/{merchantAccount}/connectedDevices", null);
+    String response =
+        resource.request(
+            null,
+            null,
+            ApiConstants.HttpMethod.GET,
+            pathParams,
+            queryParams.isEmpty() ? null : queryParams);
+
+    return ConnectedDevicesResponse.fromJson(response);
+  }
+
+  /**
+   * Get the status of a device.
+   *
+   * @param merchantAccount The unique identifier of the merchant account.
+   * @param deviceId The unique identifier of the device.
+   * @return An instance of DeviceStatus.
+   * @throws Exception when an error occurs
+   */
+  public DeviceStatusResponse getDeviceStatus(String merchantAccount, String deviceId)
+      throws Exception {
+    // Add path params
+    Map<String, String> pathParams = new HashMap<>();
+
+    if (merchantAccount == null) {
+      throw new IllegalArgumentException("Please provide the merchantAccount path parameter");
+    }
+    pathParams.put("merchantAccount", merchantAccount);
+
+    if (deviceId == null) {
+      throw new IllegalArgumentException("Please provide the deviceId path parameter");
+    }
+    pathParams.put("deviceId", deviceId);
+
+    Resource resource =
+        new Resource(
+            this, this.baseURL + "/merchants/{merchantAccount}/devices/{deviceId}/status", null);
+    String response = resource.request(null, null, ApiConstants.HttpMethod.GET, pathParams, null);
+
+    return DeviceStatusResponse.fromJson(response);
+  }
 }
--- src/test/java/com/adyen/clouddevice/CloudDeviceApiTest.java
+++ src/test/java/com/adyen/clouddevice/CloudDeviceApiTest.java
@@ -1,92 +1,181 @@
 package com.adyen.clouddevice;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+
 import com.adyen.BaseTest;
 import com.adyen.Client;
 import com.adyen.constants.ApiConstants;
 import com.adyen.model.clouddevice.*;
 import com.adyen.service.clouddevice.CloudDeviceApi;
-import org.junit.Test;
-
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
-import java.util.GregorianCalendar;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.mockito.Mockito.verify;
+import java.util.Map;
+import org.junit.Test;
 
 public class CloudDeviceApiTest extends BaseTest {
 
-	@Test
-	public void sendSync() throws Exception {
-		Client client = createMockClientFromFile("mocks/clouddevice/payment-sync-success.json");
+  @Test
+  public void sendSync() throws Exception {
+    Client client = createMockClientFromFile("mocks/clouddevice/payment-sync-success.json");
 
-		CloudDeviceApi cloudDeviceApi = new CloudDeviceApi(client);
+    CloudDeviceApi cloudDeviceApi = new CloudDeviceApi(client);
 
-		CloudDeviceApiRequest cloudDeviceApiRequest = createCloudDeviceAPIPaymentRequest();
+    CloudDeviceApiRequest cloudDeviceApiRequest = createCloudDeviceAPIPaymentRequest();
 
-		var response = cloudDeviceApi.sendSync("myMerchant", "P400Plus-123456789", cloudDeviceApiRequest);
+    var response =
+        cloudDeviceApi.sendSync("myMerchant", "P400Plus-123456789", cloudDeviceApiRequest);
 
-		assertNotNull(response);
-		assertNotNull(response.getSaleToPOIResponse());
-		assertNotNull(response.getSaleToPOIResponse().getMessageHeader());
-		assertEquals("P400Plus-123456789", response.getSaleToPOIResponse().getMessageHeader().getPOIID());
+    assertNotNull(response);
+    assertNotNull(response.getSaleToPOIResponse());
+    assertNotNull(response.getSaleToPOIResponse().getMessageHeader());
+    assertEquals(
+        "P400Plus-123456789", response.getSaleToPOIResponse().getMessageHeader().getPOIID());
 
-		verify(client.getHttpClient())
-				.request(
-						"/merchants/myMerchant/devices/P400Plus-123456789/sync",
-						cloudDeviceApiRequest.toJson(),
-						client.getConfig(),
-						false,
-						null,
-						ApiConstants.HttpMethod.POST,
-						null);
+    verify(client.getHttpClient())
+        .request(
+            "https://device-api-test.adyen.com/v1/merchants/myMerchant/devices/P400Plus-123456789/sync",
+            cloudDeviceApiRequest.toJson(),
+            client.getConfig(),
+            false,
+            null,
+            ApiConstants.HttpMethod.POST,
+            null);
+  }
 
-	}
+  @Test
+  public void sendAsync() throws Exception {
+    Client client = createMockClientFromFile("mocks/clouddevice/payment-async-success.json");
 
-	protected CloudDeviceApiRequest createCloudDeviceAPIPaymentRequest()
-			throws DatatypeConfigurationException {
-		SaleToPOIRequest saleToPOIRequest = new SaleToPOIRequest();
+    CloudDeviceApi cloudDeviceApi = new CloudDeviceApi(client);
 
-		MessageHeader messageHeader = new MessageHeader();
-		messageHeader.setProtocolVersion("3.0");
-		messageHeader.setMessageClass(MessageClassType.SERVICE);
-		messageHeader.setMessageCategory(MessageCategoryType.PAYMENT);
-		messageHeader.setMessageType(MessageType.REQUEST);
-		messageHeader.setSaleID("001");
-		messageHeader.setServiceID("001");
-		messageHeader.setPOIID("P400Plus-123456789");
+    CloudDeviceApiRequest cloudDeviceApiRequest = createCloudDeviceAPIPaymentRequest();
 
-		saleToPOIRequest.setMessageHeader(messageHeader);
+    var response =
+        cloudDeviceApi.sendAsync("myMerchant", "P400Plus-123456789", cloudDeviceApiRequest);
 
-		PaymentRequest paymentRequest = new PaymentRequest();
+    assertNotNull(response);
+    assertNotNull("ok", response);
 
-		SaleData saleData = new SaleData();
-		TransactionIdentification transactionIdentification = new TransactionIdentification();
-		transactionIdentification.setTransactionID("001");
-		OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
-		transactionIdentification.setTimeStamp(timestamp);
-		saleData.setSaleTransactionID(transactionIdentification);
+    verify(client.getHttpClient())
+        .request(
+            "https://device-api-test.adyen.com/v1/merchants/myMerchant/devices/P400Plus-123456789/async",
+            cloudDeviceApiRequest.toJson(),
+            client.getConfig(),
+            false,
+            null,
+            ApiConstants.HttpMethod.POST,
+            null);
+  }
 
-		PaymentTransaction paymentTransaction = new PaymentTransaction();
-		AmountsReq amountsReq = new AmountsReq();
-		amountsReq.setCurrency("EUR");
-		amountsReq.setRequestedAmount(BigDecimal.ONE);
-		paymentTransaction.setAmountsReq(amountsReq);
+  @Test
+  public void getConnectedDevices() throws Exception {
+    Client client = createMockClientFromFile("mocks/clouddevice/connected-devices.json");
+    CloudDeviceApi cloudDeviceApi = new CloudDeviceApi(client);
 
-		paymentRequest.setSaleData(saleData);
-		paymentRequest.setPaymentTransaction(paymentTransaction);
+    ConnectedDevicesResponse response = cloudDeviceApi.getConnectedDevices("myMerchant");
 
-		saleToPOIRequest.setPaymentRequest(paymentRequest);
+    assertNotNull(response);
+    assertEquals(2, response.getUniqueDeviceIds().size());
+    assertEquals("P400Plus-123456789", response.getUniqueDeviceIds().get(0));
+    assertEquals("V400m-123456789", response.getUniqueDeviceIds().get(1));
 
-		CloudDeviceApiRequest cloudDeviceApiRequest = new CloudDeviceApiRequest();
-		cloudDeviceApiRequest.setSaleToPOIRequest(saleToPOIRequest);
+    verify(client.getHttpClient())
+        .request(
+            "https://device-api-test.adyen.com/v1/merchants/myMerchant/connectedDevices",
+            null,
+            client.getConfig(),
+            false,
+            null,
+            ApiConstants.HttpMethod.GET,
+            null);
+  }
 
-		return cloudDeviceApiRequest;
-	}
+  @Test
+  public void getConnectedDevicesWithStore() throws Exception {
+    Client client = createMockClientFromFile("mocks/clouddevice/connected-devices.json");
+    CloudDeviceApi cloudDeviceApi = new CloudDeviceApi(client);
 
+    ConnectedDevicesResponse response = cloudDeviceApi.getConnectedDevices("myMerchant", "myStore");
+
+    assertNotNull(response);
+    assertEquals(2, response.getUniqueDeviceIds().size());
+    assertEquals("P400Plus-123456789", response.getUniqueDeviceIds().get(0));
+    assertEquals("V400m-123456789", response.getUniqueDeviceIds().get(1));
+
+    verify(client.getHttpClient())
+        .request(
+            "https://device-api-test.adyen.com/v1/merchants/myMerchant/connectedDevices",
+            null,
+            client.getConfig(),
+            false,
+            null,
+            ApiConstants.HttpMethod.GET,
+            Map.of("store", "myStore"));
+  }
+
+  @Test
+  public void getDeviceStatus() throws Exception {
+    Client client = createMockClientFromFile("mocks/clouddevice/status-device.json");
+    CloudDeviceApi cloudDeviceApi = new CloudDeviceApi(client);
+
+    DeviceStatusResponse response =
+        cloudDeviceApi.getDeviceStatus("myMerchant", "AMS1-000168242800763");
+
+    assertNotNull(response);
+    assertEquals("AMS1-000168242800763", response.getDeviceId());
+    assertEquals(DeviceStatus.ONLINE, response.getStatus());
+
+    verify(client.getHttpClient())
+        .request(
+            "https://device-api-test.adyen.com/v1/merchants/myMerchant/devices/AMS1-000168242800763/status",
+            null,
+            client.getConfig(),
+            false,
+            null,
+            ApiConstants.HttpMethod.GET,
+            null);
+  }
+
+  protected CloudDeviceApiRequest createCloudDeviceAPIPaymentRequest() {
+    SaleToPOIRequest saleToPOIRequest = new SaleToPOIRequest();
+
+    MessageHeader messageHeader = new MessageHeader();
+    messageHeader.setProtocolVersion("3.0");
+    messageHeader.setMessageClass(MessageClassType.SERVICE);
+    messageHeader.setMessageCategory(MessageCategoryType.PAYMENT);
+    messageHeader.setMessageType(MessageType.REQUEST);
+    messageHeader.setSaleID("001");
+    messageHeader.setServiceID("001");
+    messageHeader.setPOIID("P400Plus-123456789");
+
+    saleToPOIRequest.setMessageHeader(messageHeader);
+
+    PaymentRequest paymentRequest = new PaymentRequest();
+
+    SaleData saleData = new SaleData();
+    TransactionIdentification transactionIdentification = new TransactionIdentification();
+    transactionIdentification.setTransactionID("001");
+    OffsetDateTime timestamp = OffsetDateTime.now(ZoneOffset.UTC);
+    transactionIdentification.setTimeStamp(timestamp);
+    saleData.setSaleTransactionID(transactionIdentification);
+
+    PaymentTransaction paymentTransaction = new PaymentTransaction();
+    AmountsReq amountsReq = new AmountsReq();
+    amountsReq.setCurrency("EUR");
+    amountsReq.setRequestedAmount(BigDecimal.ONE);
+    paymentTransaction.setAmountsReq(amountsReq);
+
+    paymentRequest.setSaleData(saleData);
+    paymentRequest.setPaymentTransaction(paymentTransaction);
+
+    saleToPOIRequest.setPaymentRequest(paymentRequest);
+
+    CloudDeviceApiRequest cloudDeviceApiRequest = new CloudDeviceApiRequest();
+    cloudDeviceApiRequest.setSaleToPOIRequest(saleToPOIRequest);
+
+    return cloudDeviceApiRequest;
+  }
 }
