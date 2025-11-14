32759fc0770aa37a3a191401f9e8adcb6295135c
gcatanese <gcatanese@yahoo.com>
2025-11-14 08:37:45 +0100

    Improve comments related HTTP headers

--- src/main/java/com/adyen/model/RequestOptions.java
+++ src/main/java/com/adyen/model/RequestOptions.java
@@ -1,33 +1,89 @@
+/*
+ *                       ######
+ *                       ######
+ * ############    ####( ######  #####. ######  ############   ############
+ * #############  #####( ######  #####. ######  #############  #############
+ *        ######  #####( ######  #####. ######  #####  ######  #####  ######
+ * ###### ######  #####( ######  #####. ######  #####  #####   #####  ######
+ * ###### ######  #####( ######  #####. ######  #####          #####  ######
+ * #############  #############  #############  #############  #####  ######
+ *  ############   ############  #############   ############  #####  ######
+ *                                      ######
+ *                               #############
+ *                               ############
+ *
+ * Adyen Java API Library
+ *
+ * Copyright (c) 2022 Adyen B.V.
+ * This file is open source and available under the MIT license.
+ * See the LICENSE file for more info.
+ */
 package com.adyen.model;
 
 import java.util.HashMap;
 
+/**
+ * Container for optional HTTP headers that can be included in API requests.
+ * These options can be used to specify headers such as the Idempotency-Key, WWW-Authenticate
+ * or other custom headers.
+ */
 public class RequestOptions {
   private String idempotencyKey;
   private String requestedVerificationCodeHeader;
   private String wwwAuthenticateHeader;
   private HashMap<String, String> additionalServiceHeaders;
 
+  /**
+   * Set the Idempotency-Key header for the request.
+   *
+   * @param idempotencyKey The idempotency key.
+   * @return This {@link RequestOptions} instance.
+   */
   public RequestOptions idempotencyKey(String idempotencyKey) {
     this.idempotencyKey = idempotencyKey;
     return this;
   }
 
+  /**
+   * Set the Requested-Verification-Code header for the request.
+   *
+   * @param requestedVerificationCodeHeader The requested verification code.
+   * @return This {@link RequestOptions} instance.
+   */
   public RequestOptions requestedVerificationCodeHeader(String requestedVerificationCodeHeader) {
     this.requestedVerificationCodeHeader = requestedVerificationCodeHeader;
     return this;
   }
 
+  /**
+   * Set the WWW-Authenticate header for the request.
+   *
+   * @param wwwAuthenticateHeader The WWW-Authenticate header value.
+   * @return This {@link RequestOptions} instance.
+   */
   public RequestOptions wwwAuthenticateHeader(String wwwAuthenticateHeader) {
     this.wwwAuthenticateHeader = wwwAuthenticateHeader;
     return this;
   }
 
+  /**
+   * Set additional headers for the request.
+   *
+   * @param additionalServiceHeaders A map of additional headers.
+   * @return This {@link RequestOptions} instance.
+   */
   public RequestOptions additionalServiceHeaders(HashMap<String, String> additionalServiceHeaders) {
     this.additionalServiceHeaders = additionalServiceHeaders;
     return this;
   }
 
+  /**
+   * Add an additional header to the request.
+   *
+   * @param key The key of the header.
+   * @param value The value of the header.
+   * @return This {@link RequestOptions} instance.
+   */
   public RequestOptions addAdditionalServiceHeader(String key, String value) {
     if (this.additionalServiceHeaders == null) {
       this.additionalServiceHeaders = new HashMap<>();
@@ -36,34 +92,74 @@ public class RequestOptions {
     return this;
   }
 
+  /**
+   * Gets idempotency key.
+   *
+   * @return the idempotency key
+   */
   public String getIdempotencyKey() {
     return idempotencyKey;
   }
 
+  /**
+   * Sets idempotency key.
+   *
+   * @param idempotencyKey the idempotency key
+   */
   public void setIdempotencyKey(String idempotencyKey) {
     this.idempotencyKey = idempotencyKey;
   }
 
+  /**
+   * Gets requested verification code header.
+   *
+   * @return the requested verification code header
+   */
   public String getRequestedVerificationCodeHeader() {
     return requestedVerificationCodeHeader;
   }
 
+  /**
+   * Sets requested verification code header.
+   *
+   * @param requestedVerificationCodeHeader the requested verification code header
+   */
   public void setRequestedVerificationCodeHeader(String requestedVerificationCodeHeader) {
     this.requestedVerificationCodeHeader = requestedVerificationCodeHeader;
   }
 
+  /**
+   * Gets additional headers.
+   *
+   * @return the additional headers
+   */
   public HashMap<String, String> getAdditionalServiceHeaders() {
     return additionalServiceHeaders;
   }
 
+  /**
+   * Sets additional headers.
+   *
+   * @param additionalServiceHeaders the additional headers
+   */
   public void setAdditionalServiceHeaders(HashMap<String, String> additionalServiceHeaders) {
     this.additionalServiceHeaders = additionalServiceHeaders;
   }
 
+  /**
+   * Gets www authenticate header.
+   *
+   * @return the www authenticate header
+   */
   public String getWwwAuthenticateHeader() {
     return wwwAuthenticateHeader;
   }
 
+  /**
+   * Sets www authenticate header.
+   *
+   * @param wwwAuthenticateHeader the www authenticate header
+   */
   public void setWwwAuthenticateHeader(String wwwAuthenticateHeader) {
     this.wwwAuthenticateHeader = wwwAuthenticateHeader;
   }
