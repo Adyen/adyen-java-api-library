041f799378dc2bee35850a4414d84e255784f3fa
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-03-05 17:34:35 +0000

    [capital] Automated update from Adyen/adyen-openapi@37c03ec

--- src/test/java/com/adyen/DataProtectionServiceTest.java
+++ src/test/java/com/adyen/DataProtectionServiceTest.java
@@ -37,9 +37,7 @@ public class DataProtectionServiceTest extends BaseTest {
 
   @Test
   public void baseUrlOnTest() throws NoSuchFieldException, IllegalAccessException {
-    Client client = new Client(new Config()
-            .apiKey("test")
-            .environment(Environment.TEST));
+    Client client = new Client(new Config().apiKey("test").environment(Environment.TEST));
 
     DataProtectionApi dataProtectionApi = new DataProtectionApi(client);
     // get field by reflection (it is protected)
@@ -51,9 +49,7 @@ public class DataProtectionServiceTest extends BaseTest {
 
   @Test
   public void baseUrlOnLive() throws NoSuchFieldException, IllegalAccessException {
-    Client client = new Client(new Config()
-            .apiKey("test")
-            .environment(Environment.LIVE));
+    Client client = new Client(new Config().apiKey("test").environment(Environment.LIVE));
 
     DataProtectionApi dataProtectionApi = new DataProtectionApi(client);
     // get field by reflection (it is protected)
