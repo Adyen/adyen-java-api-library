5aa5a79d7e5cd53dcff2e3f78e196e110d9330d9
AdyenAutomationBot <38424300+AdyenAutomationBot@users.noreply.github.com>
2026-04-08 07:41:30 +0000

    [recurring] Automated update from Adyen/adyen-openapi@d265ad5

--- src/main/java/com/adyen/terminal/serialization/TerminalAPIGsonBuilder.java
+++ src/main/java/com/adyen/terminal/serialization/TerminalAPIGsonBuilder.java
@@ -23,24 +23,23 @@ package com.adyen.terminal.serialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import javax.xml.datatype.XMLGregorianCalendar;
 
 public final class TerminalAPIGsonBuilder {
 
-    private TerminalAPIGsonBuilder() {
-        // not meant to be initialized
-    }
+  private TerminalAPIGsonBuilder() {
+    // not meant to be initialized
+  }
 
-    public static Gson create() {
-        GsonBuilder gsonBuilder = new GsonBuilder();
-        gsonBuilder.registerTypeHierarchyAdapter(byte[].class, new ByteArrayToBase64TypeAdapter());
-        gsonBuilder.registerTypeHierarchyAdapter(
-                XMLGregorianCalendar.class, new XMLGregorianCalendarTypeAdapter());
-        gsonBuilder.registerTypeAdapterFactory(new XMLEnumAdapterFactory());
-        gsonBuilder.registerTypeAdapterFactory(new AuthenticatedDataTypeAdapterFactory());
-        gsonBuilder.disableHtmlEscaping();
+  public static Gson create() {
+    GsonBuilder gsonBuilder = new GsonBuilder();
+    gsonBuilder.registerTypeHierarchyAdapter(byte[].class, new ByteArrayToBase64TypeAdapter());
+    gsonBuilder.registerTypeHierarchyAdapter(
+        XMLGregorianCalendar.class, new XMLGregorianCalendarTypeAdapter());
+    gsonBuilder.registerTypeAdapterFactory(new XMLEnumAdapterFactory());
+    gsonBuilder.registerTypeAdapterFactory(new AuthenticatedDataTypeAdapterFactory());
+    gsonBuilder.disableHtmlEscaping();
 
-        return gsonBuilder.create();
-    }
+    return gsonBuilder.create();
+  }
 }
